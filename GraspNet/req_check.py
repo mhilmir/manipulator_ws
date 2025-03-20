09d86f168ec0daea82c54065d420d946ce55ac3a
mhilmir <rusydiansyahhilmi@gmail.com>
2025-03-20 13:50:39 +0700

    add GraspNet dev directory

---
+++ GraspNet/req_check.py
@@ -0,0 +1,17 @@
+import sys
+import torch
+import open3d
+import tensorboard
+import numpy
+import scipy
+import PIL
+import tqdm
+
+print("Python Version:", sys.version)
+print("PyTorch Version:", torch.__version__)
+print("Open3D Version:", open3d.__version__)
+print("TensorBoard Version:", tensorboard.__version__)
+print("NumPy Version:", numpy.__version__)
+print("SciPy Version:", scipy.__version__)
+print("Pillow Version:", PIL.__version__)
+print("tqdm Version:", tqdm.__version__)
